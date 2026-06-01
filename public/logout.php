614b5d197ef4af96eabab9d86461973e5bc2d69b
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-06-01 09:35:38 -0300

    doc- a página de logout.php foi comentada

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,12 @@
 <?php
 
 session_start();
+// Inicia a sessão atual para que ela possa ser encerrada.
 session_destroy ();
+// Remove todos os dados armazenados na sessão.
 header ("Location: ../index.php");
+// Redireciona o usuário para a página de login.
 exit();
+// Encerra a execução do script.
 
 ?>
