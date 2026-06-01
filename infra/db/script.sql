2d39fe25be13275c032c2cb9668497b477e2856b
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-06-01 09:31:34 -0300

    doc- comentário da página de script.sql foi concluída

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -9,3 +9,13 @@ CREATE TABLE usuarios (
 );
 
 INSERT INTO usuarios (usuario,senha) VALUE ('admin','123');
+
+-- CREATE DATABASE- cria um banco de dados.
+-- USE- seleciona qual banco será usado.
+-- CREATE TABLE- cria uma tabela.
+-- INT- tipo numérico inteiro.
+-- AUTO_INCREMENT-  aumenta automaticamente o valor do ID.
+-- PRIMARY KEY-  identifica cada registro de forma única.
+-- VARCHAR(255)- armazena textos de até 255 caracteres.
+-- NOT NULL- o campo é obrigatório.
+-- INSERT INTO  insere dados na tabela.
